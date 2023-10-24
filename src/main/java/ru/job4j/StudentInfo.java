ffa9591399e802c2cfcb485797b53e3bfd0eb2b8
Slava Osipov <skybright@mail.ru>
2023-10-25 00:31:11 +0300

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Viacheslav Osipov");
+        System.out.println("30.01.1977");
+    }
+}
