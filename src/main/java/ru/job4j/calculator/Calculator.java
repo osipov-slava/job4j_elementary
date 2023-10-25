dbf12ce0d2510d5daeade7109733acf4188aa47e
Slava Osipov <skybright@mail.ru>
2023-10-25 13:56:07 +0300

    5. Конвертер валюты [#41583]

--- src/main/java/ru/job4j/calculator/Calculator.java
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,14 +1,22 @@
 package ru.job4j.calculator;
 
 public class Calculator {
+
     public static void plus(int first, int second) {
         int result = first + second;
         System.out.println(result);
     }
 
-    public static void main(String[] args) {
-        Calculator.plus(1, 2);
-        Calculator.plus(10, 11);
-        int result3 = MathFunc.func1(100);
+    public static void minus(int first, int second) {
+        int result = first - second;
+        System.out.println(result);
     }
+
+    public static void main(String[] args) {
+        plus(1, 2);
+        plus(10, 11);
+        minus(7, 3);
+        minus(2, 6);
+    }
+
 }
